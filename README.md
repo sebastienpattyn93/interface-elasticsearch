c115e3b16642d6ad94b814f94c078c936884ff99
Charles Butler <chuck@dasroot.net>
2016-02-29 17:24:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Elasticsearch Interface
+
+> Used to connect an Elasticsearch client to an Elasticsearch charm
+
+### Usage
+
+assuming you have a `requires` block in `metadata.yaml`:
+
+     requires:
+       elasticsearch:
+         interface: elasticsearch
+
+then you can consume the cluster as follows:
+
+     @when('elasticsearch.available')
+     def connect_to_elasticsearch(elasticsearch):
+         print(elasticsearch.host())
+         print(elasticsearch.port())
+         print(elasticsearch.cluster_name())
+
+
+#### States
+
+**elasticsearch.connected** - Denotes that the client has connected to the
+elasticsearch node(s), but has not yet received the data to configure the
+connection.
+
+**elasticsearch.available** - Denotes that the client has connected and received
+all the information from the provider to make the connection.
+
+**elasticsearch.departed** - Denotes that the unit has departed from the elasticsearch
+relationship, and should be removed from any configuration files, etc.
+
+## Maintainers
+ 
+ - [Matt Bruzek](mailto:matthew.bruzek@canonical.com) &lt;matthew.bruzek@canonical.com&gt;
+ - [Charles Butler](mailto:charles.butler@canonical.com) &lt;charles.butler@canonical.com&gt;
---
+++ requires.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from charms.reactive import RelationBase
+from charms.reactive import hook
+from charms.reactive import scopes
+
+
+class ElasticSearchClient(RelationBase):
+    scope = scopes.SERVICE
+
+    # Existing elasticsearch client interface listed here:
+    # https://api.jujucharms.com/charmstore/v5/trusty/elasticsearch-13/archive/playbook.yaml
+    attr_accessors = ['host', 'port']
+
+    @hook('{requires:elasticsearch}-relation-{joined,changed}')
+    def changed(self):
+        self.set_state('{relation_name}.connected')
+        conv = self.conversation()
+        # assume we have all the data if we have the port
+        if conv.get_remote('port'):
+            conv.set_state('{relation_name}.available')
+        
+    @hook('{requires:elasticsearch}-relation-{broken, departed}')
+    def broken(self):
+        self.remove_state('{relation_name}.connected')
+        self.remove_state('{relation_name}.connected')
+        self.set_state('{relation_name}.broken')
+
+
+
+    def cluster_name(self):
+        self.get_remote('cluster-name')
